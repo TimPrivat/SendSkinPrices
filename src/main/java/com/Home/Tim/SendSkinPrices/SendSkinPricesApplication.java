aa75fe4e7316a2fde31d07e9747d87dd70f59f00
TimPrivat <timleon2701@gmail.com>
2023-10-28 17:03:00 +0200

    Initail Commit

---
+++ src/main/java/com/Home/Tim/SendSkinPrices/SendSkinPricesApplication.java
@@ -0,0 +1,13 @@
+package com.Home.Tim.SendSkinPrices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendSkinPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendSkinPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Home/Tim/SendSkinPrices/SendSkinPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Home.Tim.SendSkinPrices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendSkinPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
